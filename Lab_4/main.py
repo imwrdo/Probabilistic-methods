7c6f3ae35591f0a8f5509c331e04a6ee3b3e7745
Mykola Pozniak <87877391+imwrdo@users.noreply.github.com>
2024-05-16 14:48:46 +0200

    improved algorytm

--- Lab_4/main.py
+++ Lab_4/main.py
@@ -1,35 +1,46 @@
 import random
 from tabulate import tabulate
 
+def giveX(num):
+    licz = 0
+    sum = 0
+    prob = [0.5,0.2,0.2,0.1]
+    for i in prob:
+        sum+=i
+        if(num < sum):
+            return licz+1
+        licz+=1
+    return 4
+
+def giveY(num,prob):
+    licz = 0
+    sum = 0
+    for i in prob:
+        sum+=i
+        if(num < sum):
+            return licz+1
+        licz+=1
+    return 4
+
 def generate(randomNum):
+    probY1 = [0.2,0,0,0.8]
+    probY2 = [1,0,0,0]
+    probY3 = [0,0.5,0,0.5]
+    probY4 = [0,0,1,0]
     x = randomNum.random()
-
-    if x < 0.5:
-        xResult = 1
-    elif x < 0.7:
-        xResult = 2
-    elif x < 0.9:
-        xResult = 3
-    else:
-        xResult = 4
-
+    xResult = giveX(x)
     if xResult == 1:
         y = randomNum.random()
-        if y < 0.2:
-            yResult = 1
-        else:
-            yResult = 4
+        yResult = giveY(y,probY1)
     elif xResult == 2:
-        yResult = 1
+        y = randomNum.random()
+        yResult = giveY(y,probY2)
     elif xResult == 3:
         y = randomNum.random()
-        if y < 0.5:
-            yResult = 2
-        else:
-            yResult = 4
+        yResult = giveY(y,probY3)
     else:
-        yResult = 3
-
+        y = randomNum.random()
+        yResult = giveY(y,probY4)
     return xResult, yResult
 
 def main():
